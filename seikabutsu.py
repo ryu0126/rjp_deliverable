ab18dcd95dbfca0d10b0764e44f22a32613a4478
rick <rk22366003@ga.ttc.ac.jp>
2023-01-20 15:29:14 +0900

    'supura'

--- seikabutsu.py
+++ seikabutsu.py
@@ -5,6 +5,8 @@ from scipy import stats
 from scipy import integrate
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'Meiryo'
+import streamlit as st
+from PIL import Image
 
 anaban = pd.read_csv('data_csv/anaban.csv')
 bton = pd.read_csv('data_csv/bton.csv')
@@ -15,3 +17,10 @@ violet = pd.read_csv('data_csv/violet.csv')
 
 st.title('映画を観た感情の起伏')
 
+img = Image.open('スプラトゥーン3.jpg')
+if st.checkbox('check'):
+    st.image(img,caption='スプラトゥーン',use_column_width=True)
+
+left_column,right_column = st.beta_columns(2)
+left_column.image(img,caption='スプラトゥーン',use_column_width=True)
+right_column.image(img,caption='スプラトゥーン',use_column_width=True)
